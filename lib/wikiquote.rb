c30d2f0d7465222d465e1a5ed17b3eac1d8590af
Sammidysam <sammidysam@gmail.com>
2014-01-01 21:08:19 -0500

    Make code prettier, fix spelling errors
    Adds a space before /usr/bin/env in the shebang because it can be put there and it is pretty.
    Fixes spelling of env.
    Adds some newlines for prettiness.
    Removes a redundant comment (was mentioned twice).
    Move comment to keep line length nice.

--- lib/wikiquote.rb
+++ lib/wikiquote.rb
@@ -1,7 +1,8 @@
-#!/usr/bin/evn ruby
+#! /usr/bin/env ruby
 require 'net/http'
 require 'open-uri'
 require 'nokogiri'
+
 # The main wiki driver
 class WikiQuote
     # Get quote of the day from wikiquotes
@@ -10,9 +11,11 @@ class WikiQuote
     #   >> WikiQuote.get()
     #   => "Ruby rocks" 
     def self.get()
-        # Gets the quote of the day 
         quote_url = 'http://en.wikiquote.org/wiki/Main_Page'
-        quote_selector = "td , #mw-content-text div div:nth-child(1)" # Hope this remians ;) 
+        
+        # Hope this remains ;)
+        quote_selector = "td , #mw-content-text div div:nth-child(1)"
+        
         page = Nokogiri::HTML(open(quote_url).read)
         page.css(quote_selector)[3].text
     end
